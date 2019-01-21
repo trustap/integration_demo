c7b171a854f8242a5d6901fffa04d5f9f3775b56
Sean Kelleher <sean@trustap.com>
2019-01-21 09:46:37 +0000

    Removed unused style code

--- p2p/listing.php
+++ p2p/listing.php
@@ -9,37 +9,6 @@
     $row = $rows->fetch_assoc();
 ?>
 <html>
-    <head>
-        <style>
-.modal-mask {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, .5);
-    display: table;
-    transition: opacity .3s ease;
-}
-
-.modal-wrapper {
-    display: table-cell;
-    vertical-align: middle;
-}
-
-.modal-container {
-    width: 300px;
-    height: 300px;
-    display: block;
-    margin: 0px auto;
-    background-color: #fff;
-    border: none;
-    border-radius: 2px;
-    box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
-    transition: all .3s ease;
-}
-        </style>
-    </head>
     <body>
         <h1><?php echo htmlspecialchars($row['name']); ?></h1>
 
